86e220545a35a93a3ce60e591f0d28b3407040f5
SeungHyeon99 <bsyjsh79@gmail.com>
2024-07-29 16:14:55 +0900

    profile

--- auctionChance/routes/r_page.js
+++ auctionChance/routes/r_page.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { index, item, auction, auctiondetail } = require('../contorller/c_page');
+const { index, item, auction, auctiondetail,profile } = require('../contorller/c_page');
 
 // 최재혁
 const { secondhand } = require('../contorller/c_page');
@@ -9,6 +9,7 @@ router.get('/', index);
 router.get('/item', item);
 router.get('/auction', auction);
 router.get('/auctiondetail', auctiondetail);
+router.get('/profile',profile );
 
 
 // 중고 상품 페이지
